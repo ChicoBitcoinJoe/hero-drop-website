54b725ce5acc1ca40b17a06a751707c4718f9ac7
Joseph Reed <chicobitcoinjoe@gmail.com>
2022-02-26 17:44:09 -0800

    fixed header centering issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,25 @@
+import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 
 import logo from '../logo.png'
 
 export default function Header() {
   return <>
-    <Typography variant="h1" sx={{ display: { xs: 'none', sm: 'block' }}}>
-      Hero Drop
-    </Typography>
-    <Typography variant="h2" sx={{ display: { xs: 'block', sm: 'none' }}}>
-      Hero Drop
-    </Typography>
-    <Typography variant="h6" sx={{ color: 'rgba(255,255,255,0.5)' }} align="center">
-      a  tabletop roleplaying game focused on teamwork and creativity
-    </Typography>
-    <img src={logo} className="App-logo" alt="logo" />
+    <Grid container justifyContent="center">
+      <Typography variant="h1" sx={{ display: { xs: 'none', sm: 'block' }}}>
+        Hero Drop
+      </Typography>
+      <Typography variant="h2" sx={{ display: { xs: 'block', sm: 'none' }}}>
+        Hero Drop
+      </Typography>      
+    </Grid>
+    <Grid container justifyContent="center">
+      <Typography variant="h6" sx={{ color: 'rgba(255,255,255,0.5)' }} align="center">
+        a  tabletop roleplaying game focused on teamwork and creativity
+      </Typography>
+    </Grid>
+    <Grid container justifyContent="center">
+      <img src={logo} className="App-logo" alt="logo" />
+    </Grid>
   </>
 }
